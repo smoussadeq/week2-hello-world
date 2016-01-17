6cae79737d5bc6c363140d62aa9c5b5552b64e62
Said Moussadeq <saidmoussadeq@saids-MacBook-Pro.local>
2016-01-17 14:24:07 -0500

    Hello world

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,6 @@
-# replace the contents of this comment with your full name
+Hello World!
 
-# write a program that:
-# 1. greets the user in English
-# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
-# 3. displays the greeting in the chosen language
-# 4. exits
-
-# make sure that your code contains comments explaining your logic!
-
+Choose a language and I’ll greet you in that language!
+ 1. Spanish
+ 2. French
+ 3. German
